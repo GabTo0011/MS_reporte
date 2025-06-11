42c2525372eb063c133a26e9c41e1ee03bc5d0ef
Gabriel T <gab.toledo@duocuc.cl>
2025-06-10 21:14:01 -0400

    Inicializa proyecto >> - add file README.md >> - add file .gitignore

---
+++ gestion-reporte/src/main/java/com/perfulandia/reporte/GestionDeReporteApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeReporteApplication.class, args);
+	}
+
+}
